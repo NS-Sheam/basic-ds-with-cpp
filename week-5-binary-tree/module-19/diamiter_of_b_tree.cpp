5c004ad1af4df149601fb813bdc84e4b176b6278
NS-Sheam <123sheamfeni@gmail.com>
2025-09-28 11:38:01 +0600

    add: diameter

---
+++ week-5-binary-tree/module-19/diamiter_of_b_tree.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+
+    queue<Node *> q;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *left, *right;
+
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+
+        f->left = left;
+        f->right = right;
+
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+    return root;
+}
+
+void print_level_order(Node *root)
+{
+    if (!root)
+        return;
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+    cout << endl;
+}
+int mx = 0;
+int max_height(Node *root)
+{
+
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (!root->left && !root->right)
+        // return 0; // for max height
+        return 1; // for diameter
+
+    int l = max_height(root->left);
+    int r = max_height(root->right);
+    int diameter = l + r;
+    mx = max(mx, diameter);
+    return max(l, r) + 1;
+}
+
+int main()
+{
+
+    Node *root = input_tree();
+    // print_level_order(root);
+    int height = max_height(root);
+    cout << "height " << height << endl;
+    cout << "diameter " << mx;
+
+    return 0;
+}
