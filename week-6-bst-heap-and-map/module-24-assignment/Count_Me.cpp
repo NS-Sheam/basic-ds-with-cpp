ebe98ee3bcb63ba21fb5ab3353a69113d5a5bb77
NS-Sheam <123sheamfeni@gmail.com>
2025-10-03 12:46:00 +0600

    add: count me

---
+++ week-6-bst-heap-and-map/module-24-assignment/Count_Me.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+
+        stringstream ss(s);
+        string word;
+
+        map<string, int> mp;
+
+        string bestWord = "";
+        int bestCount = 0;
+
+        while (ss >> word)
+        {
+            mp[word]++;
+
+                        if (mp[word] > bestCount)
+            {
+                bestWord = word;
+                bestCount = mp[word];
+            }
+        }
+
+        cout << bestWord << " " << bestCount << endl;
+    }
+    return 0;
+}
